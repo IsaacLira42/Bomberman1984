518c2d68e45091169fbe6cf217f8c84640ee7ed6
Isaac Lira <isaaclira422@gmail.com>
2024-12-29 13:45:58 -0300

    Atualização do cenário, limite do mapa finalizado

--- Bomberman.asm
+++ Bomberman.asm
@@ -90,25 +90,54 @@ blocosHorizontaisBaixo:
 	addi $25, $0, 16  # Reiniciando limite de blocos horizontais
 	addi $8, $8, 3584
 	loop23132141:
-		beq $25, $0, blocosVerticais
+		beq $25, $0, contornoPretoDosBlocosLimites
 		jal blocosCinzas  # Chamada da função
 		addi $25, $25, -1
 		j loop23132141
-
-fim:
-	addi $2, $0, 10
-	syscall
+###################################################################################
+contornoPretoDosBlocosLimites:
+		lui $8, 0x1001       # Carrega os 16 bits superiores (0x1001) no registrador $8
+		ori $8, $8, 0x0E20   # Combina os 16 bits inferiores (0x0E1C) com os já existentes em $8
+		
+		lui $9, 0x0000
+		ori $9, $9, 0x0000   # Iniciando com a cor Preta
 	
+		addi $10, $0, 0    # Contador de linhas
+		addi $11, $0, 0    # Contador de colunas
+		
+		addi $25, $0, 112
+		addi $24, $0, 50
+		loopContornoHorizontal:
+			beq $25, $0, loopContornoVertical
+			addi $25, $25, -1
+			sw $9, 0($8)
+			sw $9, 25088($8)
+			addi $8, $8, 4  # Proxima posição do vetor
+			j loopContornoHorizontal
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		loopContornoVertical:
+			beq $24, $0, fim
+			addi $24, $24, -1
+			sw $9, 0($8)
+			sw $9, -452($8)
+			addi $8, $8, 512
+			j loopContornoVertical
+		
+		fim:
+			addi $2, $0, 10
+			syscall
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 	
 #####################################################################################
 # Função que desenha um quadrado do limite do mapa
